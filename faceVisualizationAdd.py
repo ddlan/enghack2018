8d1b5b5d4d3afdf13516b98269934da97a1a1dfa
ddlan <L.danny18725@gmail.com>
2018-05-26 13:48:21 -0400

    comments and i/o

--- faceVisualizationAdd.py
+++ faceVisualizationAdd.py
@@ -4,10 +4,14 @@ import glob
 import indicoio
 
 from indicoio.custom import Collection
+
 indicoio.config.api_key = '3244a46ccac6ac4cf7d163a6240b5531'
 
+collection = Collection("facial_recognition three")
+
+
 def saveFaceImage(urlin, urlout, name):
-    face = indicoio.facial_localization(urlin, sensitivity=0.05, crop=True)
+    face = indicoio.facial_localization(urlin, sensitivity=0.3, crop=True)
     print(face)
     if len(face) >= 1:
         face = face[0]
@@ -17,14 +21,12 @@ def saveFaceImage(urlin, urlout, name):
 
         collection.add_data([urlout, name])
 
-collection = Collection("facial_recognition three")
-
 
 i = 0
-for imgin in glob.iglob(r'C:\Users\ldann\Downloads\img_in\danny\*.jpg'):
+for imgin in glob.iglob(r'C:\Users\ldann\Downloads\img_in\jason\*.jpg'):
     print(imgin)
-    imgout = r"C:\Users\ldann\Downloads\img_out\danny\img" + str(i) + ".jpg"
-    saveFaceImage(imgin, imgout, "Danny")
+    imgout = r"C:\Users\ldann\Downloads\img_out\jason\img" + str(i) + ".jpg"
+    saveFaceImage(imgin, imgout, "Jason")
     print(imgout)
     i += 1
 
@@ -36,8 +38,15 @@ for imgin in glob.iglob(r'C:\Users\ldann\Downloads\img_in\jeffrey\*.jpg'):
     print(imgout)
     i += 1
 
-collection.train()
+i = 0
+for imgin in glob.iglob(r'C:\Users\ldann\Downloads\img_in\danny\*.jpg'):
+    print(imgin)
+    imgout = r"C:\Users\ldann\Downloads\img_out\danny\img" + str(i) + ".jpg"
+    saveFaceImage(imgin, imgout, "Danny")
+    print(imgout)
+    i += 1
 
+collection.train()
 collection.wait()
 
-print(collection.predict(r'C:\Users\ldann\Downloads\img_in\jeffrey.jpg'))
+print("Adding complete!")
